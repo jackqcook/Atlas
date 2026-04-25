4de86a53ef975d7e86217533b1b18dd7f56d2fa2
Quentin Cook <jackqcook@gmail.com>
2026-04-25 16:09:12 -0600

    Initial Commit

---
+++ Atlas/AtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasApp.swift
+//  Atlas
+//
+//  Created by Jack Cook on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct AtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtlasTests/AtlasTests.swift
@@ -0,0 +1,18 @@
+//
+//  AtlasTests.swift
+//  AtlasTests
+//
+//  Created by Jack Cook on 4/25/26.
+//
+
+import Testing
+
+struct AtlasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
